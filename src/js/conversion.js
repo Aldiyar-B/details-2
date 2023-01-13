e9263a20d07d758f092c36de038e3dae05c44050
SilenceLyrae <silencelyrae@gmail.com>
2023-01-13 16:47:55 +0300

    refactor: change conversion.js

--- src/js/conversion.js
+++ src/js/conversion.js
@@ -2,7 +2,7 @@ import { format, fromUnixTime } from 'date-fns';
 import { CustomError, ERROR } from './errors';
 import ELEMENT from './ui';
 
-const EXTRA_VALUE = {
+const CONVERSION = {
   DEGREE_SYMBOL: String.fromCharCode(0xb0),
   THOUSAND_MICROSECONDS: 1000,
   ABSOLUTE_ZERO: -273.15,
@@ -10,6 +10,31 @@ const EXTRA_VALUE = {
   DAY_MONTH: 'dd MMMM',
   TIME: 'time',
   DATE: 'date',
+
+  getIcon(state) {
+    const mist = ['Mist', 'Smoke', 'Haze', 'Dust', 'Fog', 'Sand', 'Ash', 'Squall', 'Tornado'];
+    const rain = ['Rain', 'Drizzle'];
+    if (mist.includes(state)) return this.getSrc(mist[0]);
+    if (rain.includes(state)) return this.getSrc(rain[0]);
+    return this.getSrc(state);
+  },
+
+  getSrc(state) {
+    const sprite = ELEMENT.SPRITE.data;
+    return `${sprite}#${state.toLowerCase()}`;
+  },
+
+  convertKelvinToCelsius(temperature) {
+    if (Number.isNaN(temperature)) throw new CustomError(ERROR.NaN);
+    return (temperature + this.ABSOLUTE_ZERO).toFixed(0);
+  },
+
+  convertTimestamp(timestamp, option) {
+    if (Number.isNaN(timestamp)) throw new CustomError(ERROR.NaN);
+    return option === this.TIME
+      ? format(fromUnixTime(timestamp), this.HOUR_MINUTE)
+      : format(fromUnixTime(timestamp), this.DAY_MONTH);
+  },
 };
 
 class BasicWeather {
@@ -17,39 +42,14 @@ class BasicWeather {
     name, main, weather, dt, sys,
   }) {
     if (name) this.city = name;
-    this.temperature = `${BasicWeather.convertKelvinToCelsius(main?.temp)}${EXTRA_VALUE.DEGREE_SYMBOL}`;
-    this.feelsLike = `${BasicWeather.convertKelvinToCelsius(main?.feels_like)}${EXTRA_VALUE.DEGREE_SYMBOL}`;
+    this.temperature = `${CONVERSION.convertKelvinToCelsius(main?.temp)}${CONVERSION.DEGREE_SYMBOL}`;
+    this.feelsLike = `${CONVERSION.convertKelvinToCelsius(main?.feels_like)}${CONVERSION.DEGREE_SYMBOL}`;
     this.state = weather[0]?.main;
-    this.icon = BasicWeather.getIcon(weather[0]?.main);
-    this.date = BasicWeather.convertTimestamp(dt, EXTRA_VALUE.DATE);
-    this.time = BasicWeather.convertTimestamp(dt, EXTRA_VALUE.TIME);
-    if (sys.sunrise) this.sunrise = BasicWeather.convertTimestamp(sys?.sunrise, EXTRA_VALUE.TIME);
-    if (sys.sunset) this.sunset = BasicWeather.convertTimestamp(sys?.sunset, EXTRA_VALUE.TIME);
-  }
-
-  static getIcon(state) {
-    const mist = ['Mist', 'Smoke', 'Haze', 'Dust', 'Fog', 'Sand', 'Ash', 'Squall', 'Tornado'];
-    const rain = ['Rain', 'Drizzle'];
-    if (mist.includes(state)) return BasicWeather.getSrc(mist[0]);
-    if (rain.includes(state)) return BasicWeather.getSrc(rain[0]);
-    return BasicWeather.getSrc(state);
-  }
-
-  static getSrc(state) {
-    const sprite = ELEMENT.SPRITE.data;
-    return `${sprite}#${state.toLowerCase()}`;
-  }
-
-  static convertKelvinToCelsius(temperature) {
-    if (Number.isNaN(temperature)) throw new CustomError(ERROR.NaN);
-    return (temperature + EXTRA_VALUE.ABSOLUTE_ZERO).toFixed(0);
-  }
-
-  static convertTimestamp(timestamp, option) {
-    if (Number.isNaN(timestamp)) throw new CustomError(ERROR.NaN);
-    return option === EXTRA_VALUE.TIME
-      ? format(fromUnixTime(timestamp), EXTRA_VALUE.HOUR_MINUTE)
-      : format(fromUnixTime(timestamp), EXTRA_VALUE.DAY_MONTH);
+    this.icon = CONVERSION.getIcon(weather[0]?.main);
+    this.date = CONVERSION.convertTimestamp(dt, CONVERSION.DATE);
+    this.time = CONVERSION.convertTimestamp(dt, CONVERSION.TIME);
+    if (sys.sunrise) this.sunrise = CONVERSION.convertTimestamp(sys?.sunrise, CONVERSION.TIME);
+    if (sys.sunset) this.sunset = CONVERSION.convertTimestamp(sys?.sunset, CONVERSION.TIME);
   }
 }
 
