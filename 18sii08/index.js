d3e4c04364ec9315ff814fadf67922daa595195f
Ilnaz <18sii08@mail.ru>
2023-01-14 11:00:48 +0300

    #26 Практика

---
+++ 18sii08/index.js
@@ -0,0 +1,25 @@
+class Storage {
+  constructor(key, value, storage) {
+    this.key = key;
+    this.value = value;
+    this.storage = storage;
+  }
+  get() {
+    return this.storage.getItem(this.key);
+  }
+  set() {
+    this.storage.setItem(this.key, this.value);
+  }
+  clear() {
+    this.storage.clear(this.key);
+  }
+  isEmpty() {
+    if (this.storage[key] === null || this.storage[key] === undefined) {
+      return true;
+    }
+  }
+}
+const names = new Storage("names", "Test", sessionStorage);
+names.get();
+names.set();
+names.isEmpty();
