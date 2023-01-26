813b13292912bfbebddac9b65abedeb9c7043d8f
Konstantin Kust <konstantinkust@gmail.com>
2023-01-27 00:09:36 +0300

    adding messages

--- popup.js
+++ popup.js
@@ -1,16 +1,16 @@
 const BUTTONS = {
-    popupName: document.getElementById('popupName'),
-    settingsButton: document.querySelector('.settings'),
-    popupNameCloseButton: document.querySelector('.popupName__close-btn'),
-    wrapper: document.querySelector('.wrapper'),
+    POPUP_NAME: document.getElementById('popupName'),
+    SETTINGS_BUTTON: document.querySelector('.settings'),
+    POPUP_NAME_CLOSE_BUTTON: document.querySelector('.popupName__close-btn'),
+    WRAPPER: document.querySelector('.wrapper'),
 };
 
-BUTTONS.settingsButton.addEventListener('click', () => {
-    BUTTONS.popupName.classList.remove('popup__hidden');
-    BUTTONS.wrapper.classList.add('wrapper__dark');
+BUTTONS.SETTINGS_BUTTON.addEventListener('click', () => {
+    BUTTONS.POPUP_NAME.classList.remove('popup__hidden');
+    BUTTONS.WRAPPER.classList.add('wrapper__dark');
 });
 
-BUTTONS.popupNameCloseButton.addEventListener('click', () => {
-    BUTTONS.popupName.classList.add('popup__hidden');
-    BUTTONS.wrapper.classList.remove('wrapper__dark');
+BUTTONS.POPUP_NAME_CLOSE_BUTTON.addEventListener('click', () => {
+    BUTTONS.POPUP_NAME.classList.add('popup__hidden');
+    BUTTONS.WRAPPER.classList.remove('wrapper__dark');
 });
