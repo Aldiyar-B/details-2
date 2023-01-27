c9f1cf1b40cf67179a42d56c37460c06b39cabb8
dimaspng <d.sagachko@gmail.com>
2023-01-27 22:16:18 +0200

    Chat, add authRequest to server

--- chat/script.js
+++ chat/script.js
@@ -17,6 +17,12 @@ ELEMENT.sendMessageForm.addEventListener('submit', (e) => {
 		sendMessage();
 });
 
+ELEMENT.loginButton.addEventListener('click', (e) => {
+		e.preventDefault();
+		showAuthorizationForm('start');
+});
+
+
 function sendMessage() {
 		if (ELEMENT.myMessageText.value === '') {
 				ELEMENT.myMessageText.placeholder = 'поле не может быть пустым';
@@ -28,7 +34,60 @@ function sendMessage() {
 
 		newMessageText.textContent = ELEMENT.myMessageText.value;
 		ELEMENT.partnerMessageTemplate.before(newMessage);
-		
+
 		ELEMENT.myMessageText.value = '';
 		ELEMENT.myMessageText.placeholder = 'сообщение..';
+}
+
+function showAuthorizationForm(status) {
+		const popUpLoginTemplate = ELEMENT.popUpLoginTemplate.content.cloneNode(true);
+		const UI_ELEMENT = {
+				title: popUpLoginTemplate.querySelector('.pop-up__title'),
+				login: popUpLoginTemplate.querySelector('div'),
+				label: popUpLoginTemplate.querySelector('label'),
+				formGetMail: popUpLoginTemplate.querySelector('form'),
+				formInput: popUpLoginTemplate.querySelector('input'),
+				formButtonSend: popUpLoginTemplate.querySelector('button'),
+				close: popUpLoginTemplate.querySelector('.pop-up__close')
+		};
+
+		if (status === 200) {
+				const previousWindow = document.querySelector('.pop-up');
+				previousWindow.style.display = 'none';
+
+				UI_ELEMENT.title.textContent = 'Подтверждение';
+				UI_ELEMENT.label.textContent = 'Код\n';
+				UI_ELEMENT.formInput.placeholder = 'введите полученный код';
+				UI_ELEMENT.label.append(UI_ELEMENT.formInput);
+				UI_ELEMENT.formButtonSend.textContent = 'Войти';
+
+		} else if (status === 'start') {
+				UI_ELEMENT.formGetMail.addEventListener('submit', (event) => {
+						event.preventDefault();
+						authRequest(event);
+				});
+		}
+		UI_ELEMENT.login.classList.add('show');
+		ELEMENT.handlerFormPushMessage.append(UI_ELEMENT.login);
+
+		UI_ELEMENT.close.addEventListener('click', (e) => {
+				e.preventDefault();
+				UI_ELEMENT.login.style.display = 'none';
+		});
+}
+
+async function authRequest(event) {
+		let gotMail = event.target[0].value;
+		const url = 'https://edu.strada.one/api/user';
+
+		try {
+				let response = await fetch(url, {
+						method: 'POST',
+						headers: {'Content-Type': 'application/json;charset=utf-8'},
+						body: JSON.stringify({email: gotMail})
+				});
+				if (response.status === 200) showAuthorizationForm(response.status);
+		} catch (error) {
+				console.log(`Возникла ошибка ${error}`);
+		}
 }
