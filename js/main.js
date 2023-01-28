5b33cc59792e7cef537f48b4607ed03e2d6371ce
Denic86 <leps6112@gmail.com>
2023-01-28 10:34:54 +0300

    Модули

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-import {messageHandler} from "./message"
+import {messageHandler} from "./message.js"
 const chatMessageForm = document.querySelector('.chat__message-form');
 document.addEventListener("DOMContentLoaded" , function (e) {
 	chatMessageForm.addEventListener('submit', messageHandler);
